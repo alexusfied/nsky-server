fbf16ee2b8ee56328045c5f6803c168cb5958d34
Alexander Fiedler <alexf137@posteo.de>
2026-03-18 23:00:32 +0100

    Cors configured

---
+++ src/main/java/org/nsky/api/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package org.nsky.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST");
+    }
+}
