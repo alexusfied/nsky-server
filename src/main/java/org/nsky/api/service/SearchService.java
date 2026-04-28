eea0d894161244215aa03f4fadb2c7963d24bc53
Alexander Fiedler <alexf137@posteo.de>
2026-04-28 21:19:41 +0200

    Search service added which can call the search engine api

---
+++ src/main/java/org/nsky/api/service/SearchService.java
@@ -0,0 +1,29 @@
+package org.nsky.api.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import tools.jackson.databind.JsonNode;
+
+import java.util.Map;
+
+@Service
+@AllArgsConstructor
+public class SearchService {
+    private final String SEARCH_ENGINE_URL = "http://localhost:8200";
+    private final WebClient client = WebClient.create(SEARCH_ENGINE_URL);
+
+    public Flux<String> performWebSearch(String query) {
+        MultiValueMap<String, String> body = MultiValueMap.fromSingleValue(Map.of("q", query, "format", "json"));
+
+        return client
+            .post()
+            .uri("/search")
+            .bodyValue(body)
+            .retrieve()
+            .bodyToFlux(JsonNode.class)
+            .map(node -> node.get("results").asString());
+    }
+}
